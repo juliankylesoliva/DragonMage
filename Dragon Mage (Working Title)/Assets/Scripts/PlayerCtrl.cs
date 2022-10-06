d1452cf8ffd5c1c0a4837400876c3e416e579ae3
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2022-10-06 16:50:22 -0500

    Completed basic movement idea

--- Dragon Mage (Working Title)/Assets/Scripts/PlayerCtrl.cs
+++ Dragon Mage (Working Title)/Assets/Scripts/PlayerCtrl.cs
@@ -8,13 +8,16 @@ public class PlayerCtrl : MonoBehaviour
 {
     /* COMPONENTS */
     Rigidbody2D rb2d;
+    SpriteRenderer charSprite;
 
     /* DRAG AND DROP */
     [Header("Drag and Drop")]
     [SerializeField] Transform groundCheckObj;
     [SerializeField] PlayerCtrlProperties mageProperties;
     [SerializeField] PlayerCtrlProperties dragonProperties;
-
+    [SerializeField] Sprite tempMageSprite;
+    [SerializeField] Sprite tempDragonSprite;
+    
     /* EDITOR VARIABLES */
     [Header("Editor Variables")]
     [SerializeField] CharacterMode startingMode = CharacterMode.MAGE;
@@ -23,33 +26,38 @@ public class PlayerCtrl : MonoBehaviour
 
     /* RUNNING VARIABLES */
     [Header("Running Variables")]
-    [SerializeField, Range(0.01f, 1f)] float acceleration = 0.5f;
-    [SerializeField, Range(0.01f, 1f)] float deceleration = 0.5f;
-    [SerializeField, Range(2f, 20f)] float topSpeed = 18f;
-    [SerializeField, Range(0.01f, 1f)] float turningSpeed = 0.5f;
+    [SerializeField] float acceleration = 0.5f;
+    [SerializeField] float deceleration = 0.5f;
+    [SerializeField] float topSpeed = 18f;
+    [SerializeField] float turningSpeed = 0.5f;
 
     /* JUMPING VARIABLES */
     [Header("Jumping Variables")]
-    [SerializeField, Range(2f, 12f)] float jumpSpeed = 4f;
-    [SerializeField, Range(0.1f, 10f)] float risingGravity = 1f;
-    [SerializeField, Range(0.1f, 10f)] float fallingGravity = 1f;
-    [SerializeField, Range(2f, 20f)] float fallSpeed = 6f;
-    [SerializeField, Range(0.01f, 1f)] float airAcceleration = 0.5f;
-    [SerializeField, Range(0.01f, 1f)] float airDeceleration = 0.5f;
-    [SerializeField, Range(0.01f, 1f)] float airTurningSpeed = 0.5f;
+    [SerializeField] float jumpSpeed = 4f;
+    [SerializeField] float risingGravity = 1f;
+    [SerializeField] float fallingGravity = 1f;
+    [SerializeField] float fallSpeed = 6f;
+    [SerializeField] float airAcceleration = 0.5f;
+    [SerializeField] float airDeceleration = 0.5f;
+    [SerializeField] float airTurningSpeed = 0.5f;
 
     [Header("Variable Jump Variables")]
     [SerializeField] bool enableVariableJumps = true;
-    [SerializeField, Range(0f, 10f)] float variableJumpDecay = 0.5f;
+    [SerializeField] float variableJumpDecay = 0.5f;
 
     [Header("Air Stalling Variables")]
     [SerializeField] bool enableAirStalling = true;
-    [SerializeField, Range(0f, 1f)] float airStallSpeed = 1f;
+    [SerializeField] float airStallSpeed = 1f;
     [SerializeField, Range(0f, 5f)] float maxAirStallTime = 3f;
 
     [Header("Midair Jump Variables")]
     [SerializeField, Range(0, 5)] int maxMidairJumps = 3;
-    [SerializeField, Range(2f, 12f)] float midairJumpSpeed = 4.25f;
+    [SerializeField] float midairJumpSpeed = 4.25f;
+
+    /* MISC VARIABLES */
+    [Header("Miscellaneous Control Variables")]
+    [SerializeField] float formChangeTime = 0.25f;
+    [SerializeField] float jumpBufferTime = 0.1f;
 
     /* SCRIPT VARIABLES */
     private CharacterMode currentMode = CharacterMode.MAGE;
@@ -57,14 +65,18 @@ public class PlayerCtrl : MonoBehaviour
     private bool jumpIsHeld = false;
     private float currentAirStallTime = 0f;
     private int currentMidairJumps = 0;
+    private bool isChangingForm = false;
+    private float jumpBufferTimeLeft = 0f;
 
     void Awake()
     {
         rb2d = this.gameObject.GetComponent<Rigidbody2D>();
+        charSprite = this.gameObject.GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
+        StartCoroutine(JumpBufferCR());
         ChangeMode(startingMode);
     }
 
@@ -87,13 +99,15 @@ public class PlayerCtrl : MonoBehaviour
 
     private void Movement()
     {
+        if (isChangingForm) { return; }
+
         if (Input.GetAxis("Horizontal") != 0f)
         {
             if ((rb2d.velocity.x * Input.GetAxis("Horizontal")) >= 0f)
             {
                 if (Mathf.Abs(rb2d.velocity.x) < topSpeed)
                 {
-                    rb2d.velocity = new Vector2(rb2d.velocity.x + ((isGrounded ? acceleration : airAcceleration) * (Input.GetAxis("Horizontal") > 0f ? 1f : -1f)), rb2d.velocity.y);
+                    rb2d.velocity = new Vector2(rb2d.velocity.x + ((isGrounded ? acceleration : airAcceleration) * (Input.GetAxis("Horizontal") > 0f ? 1f : -1f) * Time.deltaTime), rb2d.velocity.y);
                     if (Mathf.Abs(rb2d.velocity.x) > topSpeed)
                     {
                         rb2d.velocity = new Vector2(topSpeed * (Input.GetAxis("Horizontal") > 0f ? 1f : -1f), rb2d.velocity.y);
@@ -104,7 +118,7 @@ public class PlayerCtrl : MonoBehaviour
             {
                 if (rb2d.velocity.x != 0f)
                 {
-                    rb2d.velocity = new Vector2(rb2d.velocity.x + ((isGrounded ? turningSpeed : airTurningSpeed) * (Input.GetAxis("Horizontal") > 0f ? 1f : -1f)), rb2d.velocity.y);
+                    rb2d.velocity = new Vector2(rb2d.velocity.x + ((isGrounded ? turningSpeed : airTurningSpeed) * (Input.GetAxis("Horizontal") > 0f ? 1f : -1f) * Time.deltaTime), rb2d.velocity.y);
                 }
             }
         }
@@ -112,7 +126,7 @@ public class PlayerCtrl : MonoBehaviour
         {
             if (rb2d.velocity.x > 0f)
             {
-                rb2d.velocity = new Vector2(rb2d.velocity.x - (isGrounded ? deceleration : airDeceleration), rb2d.velocity.y);
+                rb2d.velocity = new Vector2(rb2d.velocity.x - ((isGrounded ? deceleration : airDeceleration) * Time.deltaTime), rb2d.velocity.y);
                 if (rb2d.velocity.x < 0f)
                 {
                     rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
@@ -120,7 +134,7 @@ public class PlayerCtrl : MonoBehaviour
             }
             else if (rb2d.velocity.x < 0f)
             {
-                rb2d.velocity = new Vector2(rb2d.velocity.x + (isGrounded ? deceleration : airDeceleration), rb2d.velocity.y);
+                rb2d.velocity = new Vector2(rb2d.velocity.x + ((isGrounded ? deceleration : airDeceleration) * Time.deltaTime), rb2d.velocity.y);
                 if (rb2d.velocity.x > 0f)
                 {
                     rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
@@ -132,20 +146,24 @@ public class PlayerCtrl : MonoBehaviour
 
     private void Jumping()
     {
+        if (isChangingForm) { return; }
+
         if (isGrounded)
         {
             currentAirStallTime = 0f;
             currentMidairJumps = 0;
 
-            if (Input.GetButtonDown("Jump"))
+            if (jumpBufferTimeLeft > 0f)
             {
+                jumpBufferTimeLeft = 0f;
                 jumpIsHeld = true;
                 rb2d.gravityScale = risingGravity;
                 rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
             }
         }
-        else if (!isGrounded && currentMidairJumps < maxMidairJumps && Input.GetButtonDown("Jump"))
+        else if (!isGrounded && currentMidairJumps < maxMidairJumps && jumpBufferTimeLeft > 0f)
         {
+            jumpBufferTimeLeft = 0f;
             jumpIsHeld = true;
             rb2d.gravityScale = risingGravity;
             rb2d.velocity = new Vector2(rb2d.velocity.x, midairJumpSpeed);
@@ -155,8 +173,6 @@ public class PlayerCtrl : MonoBehaviour
         {
             if (rb2d.velocity.y > 0f)
             {
-                rb2d.gravityScale = risingGravity;
-
                 if (jumpIsHeld && !Input.GetButton("Jump"))
                 {
                     jumpIsHeld = false;
@@ -164,7 +180,7 @@ public class PlayerCtrl : MonoBehaviour
 
                 if (enableVariableJumps && !jumpIsHeld)
                 {
-                    rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y - variableJumpDecay);
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y - (variableJumpDecay * Time.deltaTime));
                 }
             }
             else
@@ -197,8 +213,10 @@ public class PlayerCtrl : MonoBehaviour
 
     private void FormChange()
     {
-        if (Input.GetButtonDown("Change Form"))
+        if (!isChangingForm && Input.GetButtonDown("Change Form"))
         {
+            StartCoroutine(FormFreeze());
+
             if (currentMode == CharacterMode.MAGE)
             {
                 ChangeMode(CharacterMode.DRAGON);
@@ -243,6 +261,43 @@ public class PlayerCtrl : MonoBehaviour
     private void ChangeMode(CharacterMode mode)
     {
         SetCtrlProperties(mode == CharacterMode.MAGE ? mageProperties : dragonProperties);
+        charSprite.sprite = (mode == CharacterMode.MAGE ? tempMageSprite : tempDragonSprite);
         currentMode = mode;
+    }
+
+    private IEnumerator FormFreeze()
+    {
+        isChangingForm = true;
+
+        float prevGravityScale = rb2d.gravityScale;
+        Vector2 prevVelocity = rb2d.velocity;
+
+        rb2d.gravityScale = 0f;
+        rb2d.velocity = Vector2.zero;
+
+        yield return new WaitForSeconds(formChangeTime);
+
+        rb2d.gravityScale = prevGravityScale;
+        rb2d.velocity = prevVelocity;
+
+        isChangingForm = false;
+    }
+
+    private IEnumerator JumpBufferCR()
+    {
+        while (true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                jumpBufferTimeLeft = jumpBufferTime;
+            }
+
+            if (jumpBufferTimeLeft > 0f)
+            {
+                jumpBufferTimeLeft -= Time.deltaTime;
+            }
+
+            yield return null;
+        }
     }
 }
--- Dragon Mage (Working Title)/Assets/Scripts/Scriptable Objects/PlayerCtrlProperties.cs
+++ Dragon Mage (Working Title)/Assets/Scripts/Scriptable Objects/PlayerCtrlProperties.cs
@@ -7,53 +7,53 @@ public class PlayerCtrlProperties : ScriptableObject
 {
     /* RUNNING VARIABLES */
     [Header("Running Variables")]
-    [SerializeField, Range(0.01f, 1f)] float _acceleration = 0.5f;
+    [SerializeField] float _acceleration = 0.5f;
     public float acceleration { get { return _acceleration; } }
 
-    [SerializeField, Range(0.01f, 1f)] float _deceleration = 0.5f;
+    [SerializeField] float _deceleration = 0.5f;
     public float deceleration { get { return _deceleration; } }
 
-    [SerializeField, Range(2f, 20f)] float _topSpeed = 18f;
+    [SerializeField] float _topSpeed = 18f;
     public float topSpeed { get { return _topSpeed; } }
 
-    [SerializeField, Range(0.01f, 1f)] float _turningSpeed = 0.5f;
+    [SerializeField] float _turningSpeed = 0.5f;
     public float turningSpeed { get { return _turningSpeed; } }
 
     /* JUMPING VARIABLES */
     [Header("Jumping Variables")]
-    [SerializeField, Range(2f, 12f)] float _jumpSpeed = 4f;
+    [SerializeField] float _jumpSpeed = 4f;
     public float jumpSpeed { get { return _jumpSpeed; } }
 
-    [SerializeField, Range(0.1f, 10f)] float _risingGravity = 1f;
+    [SerializeField] float _risingGravity = 1f;
     public float risingGravity { get { return _risingGravity; } }
 
-    [SerializeField, Range(0.1f, 10f)] float _fallingGravity = 1f;
+    [SerializeField] float _fallingGravity = 1f;
     public float fallingGravity { get { return _fallingGravity; } }
 
-    [SerializeField, Range(2f, 20f)] float _fallSpeed = 6f;
+    [SerializeField] float _fallSpeed = 6f;
     public float fallSpeed { get { return _fallSpeed; } }
 
-    [SerializeField, Range(0.01f, 1f)] float _airAcceleration = 0.5f;
+    [SerializeField] float _airAcceleration = 0.5f;
     public float airAcceleration { get { return _airAcceleration; } }
 
-    [SerializeField, Range(0.01f, 1f)] float _airDeceleration = 0.5f;
+    [SerializeField] float _airDeceleration = 0.5f;
     public float airDeceleration { get { return _airDeceleration; } }
 
-    [SerializeField, Range(0.01f, 1f)] float _airTurningSpeed = 0.5f;
+    [SerializeField] float _airTurningSpeed = 0.5f;
     public float airTurningSpeed { get { return _airTurningSpeed; } }
 
     [Header("Variable Jump Variables")]
     [SerializeField] bool _enableVariableJumps = true;
     public bool enableVariableJumps { get { return _enableVariableJumps; } }
 
-    [SerializeField, Range(0f, 10f)] float _variableJumpDecay = 0.5f;
+    [SerializeField] float _variableJumpDecay = 0.5f;
     public float variableJumpDecay { get { return _variableJumpDecay; } }
 
     [Header("Air Stalling Variables")]
     [SerializeField] bool _enableAirStalling = true;
     public bool enableAirStalling { get { return _enableAirStalling; } }
 
-    [SerializeField, Range(0f, 1f)] float _airStallSpeed = 1f;
+    [SerializeField] float _airStallSpeed = 1f;
     public float airStallSpeed { get { return _airStallSpeed; } }
 
     [SerializeField, Range(0f, 5f)] float _maxAirStallTime = 3f;
@@ -63,6 +63,6 @@ public class PlayerCtrlProperties : ScriptableObject
     [SerializeField, Range(0, 5)] int _maxMidairJumps = 3;
     public int maxMidairJumps { get { return _maxMidairJumps; } }
 
-    [SerializeField, Range(2f, 12f)] float _midairJumpSpeed = 4.25f;
+    [SerializeField] float _midairJumpSpeed = 4.25f;
     public float midairJumpSpeed { get { return _midairJumpSpeed; } }
 }
