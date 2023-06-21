2e0e1c52643e39cb6e0ea80388e2bdf5bd6ff249
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2023-06-21 16:08:54 -0500

    Added Medallion Functionality

---
+++ Dragon Mage (Working Title)/Assets/Scripts/MedalPickup.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MedalType { BLANK, NEUTRAL, MAGIC, DRAGON }
+
+public class MedalPickup : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField] float nonNeutralRatio = 2f;
+
+    void Awake()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (MedalFragment.totalFragments >= Level.FragmentsNeededForMedal)
+        {
+            if (MedalFragment.dragonFragments == 0 || (MedalFragment.mageFragments / MedalFragment.dragonFragments) >= nonNeutralRatio)
+            {
+                animator.Play("MagicSpin");
+            }
+            else if (MedalFragment.mageFragments == 0 || (MedalFragment.dragonFragments / MedalFragment.mageFragments) >= nonNeutralRatio)
+            {
+                animator.Play("DragonSpin");
+            }
+            else
+            {
+                animator.Play("NeutralSpin");
+            }
+        }
+        else
+        {
+            animator.Play("BlankSpin");
+        }
+    }
+}
