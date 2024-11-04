9424af6057ae193486651c511c74671c456d3500
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2024-11-04 11:10:37 -0600

    wav compressions

--- Godot Fallback/Scripts/Utility Scripts/PackSplitLoad.gd
+++ Godot Fallback/Scripts/Utility Scripts/PackSplitLoad.gd
@@ -1,7 +1,12 @@
 extends Node
 
 func _ready():
-	print_debug(ProjectSettings.load_resource_pack("res://sounds.pck"))
-	print_debug(ProjectSettings.load_resource_pack("res://ch0_music.pck"))
-	print_debug(ProjectSettings.load_resource_pack("res://ch1_music.pck"))
+	while (!ProjectSettings.load_resource_pack("res://ch0_music.pck")):
+		pass
+	print_debug("ch0_music.pck loaded!")
+	
+	while (!ProjectSettings.load_resource_pack("res://ch1_music.pck")):
+		pass
+	print_debug("ch1_music.pck loaded!")
+	
 	get_tree().change_scene_to_file("res://Misc Scenes/TitleScreen.tscn")
