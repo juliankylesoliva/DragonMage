d8ac96fdd9ae0620a2a7a1591487d35c4638a45b
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2025-01-21 11:24:47 -0600

    Added some cutscene camera functionality
    - The ledge nudge will no longer activate if the player is holding a direction that goes against the nudge direction.

--- Godot Fallback/Scripts/Player/Hub Scripts/PlayerCollisions.gd
+++ Godot Fallback/Scripts/Player/Hub Scripts/PlayerCollisions.gd
@@ -186,10 +186,10 @@ func do_ledge_nudge(custom_ease : float = 0):
 		var is_right_grounded : bool = (get_distance_to_ground(1) <= hub.char_body.floor_snap_length)
 		
 		if (!is_middle_grounded and (is_left_grounded or is_right_grounded) and (!is_left_grounded or !is_right_grounded)):
-			if (!is_left_grounded and is_right_grounded):
+			if (!is_left_grounded and is_right_grounded and hub.get_input_vector().x >= 0):
 				var calculated_nudge_distance : float = (((hub.raycast_ledge_r.get_collision_point().x if hub.raycast_ledge_r.is_colliding() else hub.raycast_ledge_r.target_position.x) - hub.raycast_ledge_r.global_position.x) * (ledge_nudge_easing if custom_ease <= 0 else abs(custom_ease)))
 				hub.char_body.position.x += (calculated_nudge_distance if calculated_nudge_distance > 0 and calculated_nudge_distance <= max_nudge_distance else max_nudge_distance)
-			elif (is_left_grounded and !is_right_grounded):
+			elif (is_left_grounded and !is_right_grounded and hub.get_input_vector().x <= 0):
 				var calculated_nudge_distance = (((hub.raycast_ledge_l.get_collision_point().x if hub.raycast_ledge_l.is_colliding() else hub.raycast_ledge_l.target_position.x) - hub.raycast_ledge_l.global_position.x) * (ledge_nudge_easing if custom_ease <= 0 else abs(custom_ease)))
 				hub.char_body.position.x += (calculated_nudge_distance if calculated_nudge_distance < 0 and calculated_nudge_distance >= -max_nudge_distance else -max_nudge_distance)
 			else:
--- Godot Fallback/Scripts/Utility Scripts/CutsceneCamera.gd
+++ Godot Fallback/Scripts/Utility Scripts/CutsceneCamera.gd
@@ -4,10 +4,62 @@ extends Node
 
 @export var path : Path2D
 
-@export var path_follow : PathFollow2D
+@export var follow : PathFollow2D
 
 @export var remote_transform : RemoteTransform2D
 
-func set_curve(c : Curve2D):
-	path.set_curve(c)
-	path_follow.set_progress_ratio(0)
+var is_moving : bool = false
+
+var travel_time : float = 0
+
+var disable_after_movement : bool = false
+
+func _physics_process(delta):
+	if (is_moving):
+		if (!camera.is_enabled() or !camera.is_current()):
+			self.set_enabled(true)
+		follow.progress_ratio = move_toward(follow.progress_ratio, 1.0, delta / travel_time)
+		if (follow.progress_ratio >= 1):
+			finish_movement()
+
+func set_enabled(b : bool):
+	camera.set_enabled(b)
+	if (b):
+		camera.make_current()
+
+func move_camera_from_to(from : Vector2, to : Vector2, time : float, disable_after : bool = false):
+	if (is_moving):
+		return
+	
+	remote_transform.set_update_position(false)
+	path.curve.clear_points()
+	path.curve.add_point(from)
+	path.curve.add_point(to)
+	follow.set_progress_ratio(0)
+	
+	remote_transform.set_update_position(true)
+	travel_time = time
+	is_moving = true
+	disable_after_movement = disable_after
+
+func move_camera_to(dest : Vector2, time : float, disable_after : bool = false):
+	if (is_moving):
+		return
+	
+	remote_transform.set_update_position(false)
+	path.curve.clear_points()
+	path.curve.add_point(camera.global_position)
+	path.curve.add_point(dest)
+	follow.set_progress_ratio(0)
+	
+	remote_transform.set_update_position(true)
+	travel_time = time
+	is_moving = true
+	disable_after_movement = disable_after
+
+func finish_movement():
+	follow.set_progress_ratio(1)
+	remote_transform.set_update_position(false)
+	is_moving = false
+	if (disable_after_movement):
+		self.set_enabled(false)
