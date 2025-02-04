800bc36e41147f9c04cac7361f7e11bc88556ac0
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2025-02-03 22:41:05 -0600

    Text / sprite adjustments
    - Added walking sprite silhouette transition to level intro

--- Godot Fallback/Scripts/UI Scripts/LevelIntroScreen.gd
+++ Godot Fallback/Scripts/UI Scripts/LevelIntroScreen.gd
@@ -8,9 +8,17 @@ signal intro_finished
 
 @export var bg_combine_duration : float = 0.5
 
+@export var sprite_travel_distance : float = 832
+
+@export var sprite_move_duration : float = 3.0
+
 @export var blue_bg : Sprite2D
 
 @export var orange_bg : Sprite2D
+
+@export var mage_silhouette : AnimatedSprite2D
+
+@export var dragon_silhouette : AnimatedSprite2D
 
 @export var chapter_char : String = "#"
 
@@ -30,11 +38,13 @@ signal intro_finished
 
 @export var subtitle_format : String = "[right]~ {sub} -"
 
-@export var start_message_format : String = "[center]Go for it, {name}!"
+@export var start_message_format : String = "[center]{cheer}, {name}!"
 
 @export var mage_name_image : String = "[img=93x42]res://Sprites/UI/MagliNameSprite.png[/img]"
 
 @export var dragon_name_image : String = "[img=147x21]res://Sprites/UI/DraelynNameSprite.png[/img]"
+
+@export var start_cheers : Array[String]
 
 @export var chapter_header_label : RichTextLabel
 
@@ -50,8 +60,10 @@ func _ready():
 	PauseHandler.game_paused.connect(hide_level_name_on_pause)
 
 func do_level_intro():
-	slide_from_offset(blue_bg, Vector2.LEFT * screen_width, bg_combine_duration)
-	slide_from_offset(orange_bg, Vector2.RIGHT * screen_width, bg_combine_duration)
+	slide_sprite_from_offset(blue_bg, Vector2.LEFT * screen_width, bg_combine_duration)
+	slide_sprite_from_offset(orange_bg, Vector2.RIGHT * screen_width, bg_combine_duration)
+	slide_to_offset(mage_silhouette, Vector2.RIGHT * sprite_travel_distance, sprite_move_duration)
+	slide_to_offset(dragon_silhouette, Vector2.RIGHT * sprite_travel_distance, sprite_move_duration)
 	await get_tree().create_timer(0.25).timeout
 	chapter_header_label.text = chapter_section_format.format({"chapter" : chapter_char.substr(0, 1), "section" : section_char.substr(0, 1)})
 	slide_control_from_offset(chapter_header_label, Vector2.LEFT * screen_width, bg_combine_duration)
@@ -64,13 +76,13 @@ func do_level_intro():
 	level_subtitle_label.text = subtitle_format.format({"sub" : subtitle_text})
 	slide_control_from_offset(level_subtitle_label, Vector2.RIGHT * screen_width, bg_combine_duration)
 	await get_tree().create_timer(0.5).timeout
-	start_message_label.text = start_message_format.format({"name" : (dragon_name_image if FormSelectionHelper.is_dragon_selected() else mage_name_image)})
+	start_message_label.text = start_message_format.format({"cheer" : start_cheers.pick_random(), "name" : (dragon_name_image if FormSelectionHelper.is_dragon_selected() else mage_name_image)})
 	slide_control_from_offset(start_message_label, Vector2.LEFT * screen_width, bg_combine_duration)
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(2.0).timeout
 	slide_control_to_offset(start_message_label, Vector2.RIGHT * screen_width, bg_combine_duration)
 	await get_tree().create_timer(0.5).timeout
-	slide_to_offset(blue_bg, Vector2.LEFT * screen_width, bg_combine_duration)
-	slide_to_offset(orange_bg, Vector2.RIGHT * screen_width, bg_combine_duration)
+	slide_sprite_to_offset(blue_bg, Vector2.LEFT * screen_width, bg_combine_duration)
+	slide_sprite_to_offset(orange_bg, Vector2.RIGHT * screen_width, bg_combine_duration)
 	intro_finished.emit()
 	await get_tree().create_timer(0.75).timeout
 	slide_control_to_offset(chapter_header_label, Vector2.LEFT * screen_width, bg_combine_duration)
@@ -115,6 +127,26 @@ func slide_control_to_offset(node : Control, offset : Vector2, time : float):
 		lerp_value = move_toward(lerp_value, 1, get_physics_process_delta_time() / time)
 		node.position.x = lerp(target_position.x - offset.x, target_position.x, lerp_value)
 		node.position.y = lerp(target_position.y - offset.y, target_position.y, lerp_value)
+		await get_tree().process_frame
+	node.set_visible(false)
+
+func slide_sprite_from_offset(node : Sprite2D, offset : Vector2, time : float):
+	var target_position : Vector2 = node.offset
+	var lerp_value : float = 0
+	node.set_visible(true)
+	while (lerp_value < 1):
+		lerp_value = move_toward(lerp_value, 1, get_physics_process_delta_time() / time)
+		node.offset.x = lerp(target_position.x + offset.x, target_position.x, lerp_value)
+		node.offset.y = lerp(target_position.y + offset.y, target_position.y, lerp_value)
+		await get_tree().process_frame
+
+func slide_sprite_to_offset(node : Sprite2D, offset : Vector2, time : float):
+	var target_position : Vector2 = (node.offset + offset)
+	var lerp_value : float = 0
+	while (lerp_value < 1):
+		lerp_value = move_toward(lerp_value, 1, get_physics_process_delta_time() / time)
+		node.offset.x = lerp(target_position.x - offset.x, target_position.x, lerp_value)
+		node.offset.y = lerp(target_position.y - offset.y, target_position.y, lerp_value)
 		await get_tree().process_frame
 	node.set_visible(false)
 
