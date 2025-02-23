499f8560fd590a2894ea36e4cc05c4b62c5b3060
juliankylesoliva <68832077+juliankylesoliva@users.noreply.github.com>
2025-02-23 09:45:15 -0600

    added autoplay delay for scene changes

--- Godot Fallback/Scripts/UI Scripts/LevelMusicPlayer.gd
+++ Godot Fallback/Scripts/UI Scripts/LevelMusicPlayer.gd
@@ -8,10 +8,24 @@ class_name LevelMusicPlayer
 
 @export var silent_volume : float = -80
 
+@export var delay_autoplay : bool = false
+
+@export var delay_autoplay_time : float = 0.25
+
 var is_fading_out : bool = false
 
 func _ready():
 	volume_db = starting_volume
+	if (delay_autoplay and !autoplay):
+		do_delayed_autoplay()
+
+func do_delayed_autoplay():
+	if (playing):
+		return
+	
+	await get_tree().create_timer(delay_autoplay_time).timeout
+	if (!playing):
+		play()
 
 func restart_music():
 	play()
